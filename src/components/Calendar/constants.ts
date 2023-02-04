05ca48a145e88bf41ef6895a60effb3a3e3df9a3
rv.ivanushkin@gmail.com <rv.ivanushkin@gmail.com>
2023-02-04 23:41:58 +0400

    feat: add constants for forecast

--- src/components/Calendar/constants.ts
+++ src/components/Calendar/constants.ts
@@ -27,3 +27,17 @@ export const MONTHS = [
 export const MONTHS_COMPACT = MONTHS.map((name) => name.slice(0, 3))
 
 export const CALENDAR_GRID = 42
+
+export const FORECAST_QUERY_PARAMS = {
+  latitude: 40.38,
+  longitude: 49.89,
+  daily: [
+    'temperature_2m_max',
+    'temperature_2m_min',
+    'sunrise',
+    'sunset',
+    'windspeed_10m_max',
+  ],
+  timezone: 'auto',
+  timeformat: 'unixtime',
+}
