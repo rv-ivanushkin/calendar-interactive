995028800d86f027f1744e4b4204827a228331f1
rv.ivanushkin@gmail.com <rv.ivanushkin@gmail.com>
2023-02-04 23:42:41 +0400

    feat: add render forecast info

--- src/components/Calendar/CalendarCell.tsx
+++ src/components/Calendar/CalendarCell.tsx
@@ -1,22 +1,70 @@
 import { Button, Typography } from '@mui/material'
 import React, { useContext } from 'react'
+import ThermostatIcon from '@mui/icons-material/Thermostat'
+import AirIcon from '@mui/icons-material/Air'
+import WbSunnyIcon from '@mui/icons-material/WbSunny'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
 import { CalendarContext } from './context'
-import { CalendarCellStyled } from './style'
-import { BaseNode } from './types'
+import {
+  CalendarCellStyled,
+  ForecastStyled,
+  ForecastWithToParamsStyled,
+} from './style'
+import { BaseNodeWithForecast } from './types'
 
 export interface CalendarCellProps {
-  cell: BaseNode
+  cell: BaseNodeWithForecast
 }
 
 export const CalendarCell = ({ cell }: CalendarCellProps) => {
   const { compact } = useContext(CalendarContext)
   const typographyVariant = compact ? 'caption' : 'h6'
-  const { date } = cell
+  const {
+    date,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    temperature_2m_max,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    temperature_2m_min,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    windspeed_10m_max,
+    sunrise,
+    sunset,
+  } = cell
 
   return (
     <CalendarCellStyled compact={compact}>
       <Button>
         <Typography variant={typographyVariant}>{date}</Typography>
+        <ForecastStyled>
+          {sunrise && (
+            <ForecastWithToParamsStyled>
+              <WbSunnyIcon fontSize="small" />
+              <Typography variant="caption">
+                {new Date(Number(sunrise) * 1000).getHours()}
+              </Typography>
+            </ForecastWithToParamsStyled>
+          )}
+          {sunset && (
+            <ForecastWithToParamsStyled>
+              <DarkModeIcon fontSize="small" />
+              <Typography variant="caption">
+                {new Date(Number(sunset) * 1000).getHours()}
+              </Typography>
+            </ForecastWithToParamsStyled>
+          )}
+          {windspeed_10m_max && (
+            <ForecastWithToParamsStyled>
+              <AirIcon fontSize="small" />
+              <Typography variant="caption">{windspeed_10m_max}</Typography>
+            </ForecastWithToParamsStyled>
+          )}
+          {temperature_2m_max && (
+            <ForecastWithToParamsStyled>
+              <ThermostatIcon fontSize="small" />
+              <Typography variant="caption">{temperature_2m_max} C</Typography>
+            </ForecastWithToParamsStyled>
+          )}
+        </ForecastStyled>
       </Button>
     </CalendarCellStyled>
   )
